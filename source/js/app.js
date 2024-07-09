05a1f00d1941a7b06be7923c6b5ebd23538642d9
Tim Carry <tim@pixelastic.com>
2024-07-09 22:28:22 +0200

    Add fancybox on all pictures

--- source/js/app.js
+++ source/js/app.js
@@ -1,2 +1,17 @@
-$('.js-fancybox').fancybox();
+// Fancybox
+$('.c-session--content img').each((index, element) => {
+  const $img = $(element);
+  const url = $img.attr('src');
+  $(element).wrap(
+    `<a class="js-fancybox" href="${url}" rel="pictures-in-text">`
+  );
+});
+$('.js-fancybox').fancybox({
+  helpers: {
+    overlay: {
+      locked: false,
+    },
+  },
+});
+
 $('.js-player').mediaelementplayer();
